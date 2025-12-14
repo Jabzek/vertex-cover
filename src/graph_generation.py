8fd9bf76be5df63aeaf1706d79a2871b450b5bd2
Jacek Szambelańczyk <jacekszambelanczyk@gmail.com>
2025-12-14 12:06:44 +0100

    Adding graph generation

--- src/graph_generation.py
+++ src/graph_generation.py
@@ -1,11 +1,14 @@
 import random
-import itertools
+from pathlib import Path
+from dataclasses import dataclass, field, asdict
+import json
 
+@dataclass
 class Graph:
-    def __init__(self, n):
-        self.nodes = n
-        self.nodes_weight = []
-        self.edges = []
+    nodes: int
+    size: str
+    nodes_weight: list[int] = field(default_factory=list)
+    edges: list[tuple[int, int]] = field(default_factory=list)
 
     def add_edge(self, tuple_):
         self.edges.append(tuple_)
@@ -13,10 +16,26 @@ class Graph:
     def add_weight(self, weight):
         self.nodes_weight.append(weight)
 
+    def data_to_dict(self):
+        return asdict(self)
 
-def draw_edges(graph):
+
+def draw_edges(graph, number_of_edges):
     nodes = graph.nodes
-    edges = itertools.combinations(range(nodes), 2)
+    i = 0
+
+    while i < number_of_edges:
+        a = random.randrange(0, nodes)
+        b = random.randrange(0, nodes)
+
+        if a == b:
+            continue
+        elif a > b:
+            a, b = b, a
+
+        if (a, b) not in graph.edges:
+            graph.add_edge((a, b))
+            i += 1
 
 
 def draw_weight(graph):
@@ -26,17 +45,46 @@ def draw_weight(graph):
         graph.add_weight(w)
 
 
-def generation():
+def save_to_json(new_data, data_file, size):
+    if data_file.exists():
+        with open(data_file, "r") as f:
+            data = json.load(f)
+    else:
+        data = {"small": [],
+                "medium": [],
+                "big": []}
+    
+    data[size].append(new_data)
+
+    with open(data_file, "w") as f:
+        json.dump(data, f, indent=2)
+
+
+def data_generator(file_name):
     graphs = {"small":((18, 60), (19, 50), (20, 20), (21, 70), (22, 40)),
               "medium": ((40, 70), (60, 50), (80, 20), (90, 60), (100, 30)),
                "big": ((150, 50), (200, 30), (250, 20), (300, 15), (350, 10))}
+
+    main_folder = Path(__file__).resolve().parent.parent
+    data_folder = main_folder / "data"
+    data_folder.mkdir(exist_ok=True)
+    data_file = data_folder / file_name
     
+    if data_file.exists():
+        data_file.unlink()
+
     for size in ("small", "medium", "big"):
         for tuple_ in graphs[size]:
             nodes = tuple_[0]
             saturation = tuple_[1]
+            
             all_edges = nodes * (nodes - 1) // 2
             number_of_edges = saturation * all_edges // 100 
-            graph = Graph(nodes)
-            draw_edges(number_of_edges, graph)
+            graph = Graph(nodes, size)
+            
+            draw_edges(graph, number_of_edges)
             draw_weight(graph)
+            new_data = graph.data_to_dict()
+            save_to_json(new_data, data_file, size)
+
+
--- src/main.py
+++ src/main.py
@@ -1,18 +1,23 @@
 import argparse
-
-
+from graph_generation import data_generator
 
 
 def main():
     parser = argparse.ArgumentParser()
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("--generate", action="store_true", help="Graph generation")
+    group.add_argument("--generate", type=str, metavar="File_name", 
+                       help="Generate graphs and save them to a file. " \
+                       "Requires the file name without the file type. " \
+                       "If the file already exists its contents will be overwritten.")
     group.add_argument("--run", action="store_true", help="Run algorithms on the generated graphs")
     
     args = parser.parse_args()
 
     if args.generate:
-        print("Graph generate")
+        file_name = args.generate
+        file_json = file_name + ".json"
+        data_generator(file_json)
+        print(f"Data has been generated and saved to the {file_name}.json.")
     elif args.run:
         print("Run algorithms")
 
